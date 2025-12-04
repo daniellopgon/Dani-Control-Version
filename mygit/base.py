1790f5ff2967631976b33a419aee20580f46c1ae
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2025-12-04 10:47:37 +0100

    \Dani-Control-Version
    - Add write-tree CLI command to store entire directory trees
    - Implement write_tree() function in base module using os.scandir()
    - Add recursive traversal of directories and subdirectories
    - Implement is_ignored() function to filter .ugit directory
    - Hash and store each file as blob object in object database
    - Display OID alongside file path for each stored file

--- mygit/base.py
+++ mygit/base.py
@@ -7,10 +7,16 @@ def is_ignored(path):
     return '.ugit' in path.replace('\\', '/').split('/')
 
 
-def write_tree():
-    for root, _, filenames in os.walk('.'):
-        for filename in filenames:
-            path = os.path.join(root, filename)
-            if is_ignored(path):
+def write_tree(directory='.'):
+    with os.scandir(directory) as it:
+        for entry in it:
+            full = f'{directory}/{entry.name}'
+            if is_ignored(full):
                 continue
-            print(path)
+            if entry.is_file(follow_symlinks=False):
+                with open(full, 'rb') as f:
+                    print(data.hash_object(f.read()), full)
+            elif entry.is_dir(follow_symlinks=False):
+                write_tree(full)
+    
+    # TODO actually create the tree object
